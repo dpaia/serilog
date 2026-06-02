a9357361fe99331b06a9141d3448a6f156edd950
michaelmedvinskiy <michael.medvinskiy@gmail.com>
2026-06-02 16:52:47 +0200

    [ee-bench] Add solution JsonFormatter_Format_68_72

--- test/Serilog.Tests/Formatting/Json/JsonFormatterTestsMethodCount.cs
+++ test/Serilog.Tests/Formatting/Json/JsonFormatterTestsMethodCount.cs
@@ -10,6 +10,6 @@ public class JsonFormatterTestsMethodCount
     {
         var flags = BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly;
         var methods = typeof(JsonFormatterTests).GetMethods(flags);
-        Assert.Equal(23, methods.Length);
+         Assert.Equal(24, methods.Length);
     }
 }
